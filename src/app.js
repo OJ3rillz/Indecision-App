6354f830523931072e8cdf8ecc742e8e4d0bb8a5
OJ3rillz <timitrilli@gmail.com>
2019-06-25 18:56:20 +0100

    add progress and do challenge

--- src/app.js
+++ src/app.js
@@ -15,29 +15,44 @@ const onFormSubmit = (e) => {
   if (option) {
         app.options.push(option);
         e.target.elements.option.value = ' ';
+        render();
   }
 };
-
-const template = (
- <div>
-      <h1>{app.title}</h1>
-      {app.subtitle && <p>{app.subtitle}</p>}
-      <p>{app.options.length > 0 ? 'Here are you options'  :  'No options'}</p>
-      <p>{app.options.length}</p>
-      <ol>
-         <li>Item one</li>
-         <li>Item two</li>
-      </ol>
-
-      <form onSubmit={onFormSubmit}>
-          <input type="text" name="option"></input>
-          <button>Add Option</button>
-      </form>
-
-      </div>
-);
+const onRemoveAll = () => {
+      app.options =[];
+      render();
+}
 
 
 const appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
 
+const render =  () => {
+const template = (
+      <div>
+           <h1>{app.title}</h1>
+           {app.subtitle && <p>{app.subtitle}</p>}
+           <p>{app.options.length > 0 ? 'Here are you options'  :  'No options'}</p>
+           <p>{app.options.length}</p>
+           <button onClick={onRemoveAll}>Remove All</button>
+           <ol>
+              <li>Item one</li>
+              <li>Item two</li>
+           </ol>
+     
+           <form onSubmit={onFormSubmit}>
+               <input type="text" name="option"></input>
+               <button>Add Option</button>
+           </form>
+     
+           </div>
+     );
+     
+
+
+
+
+
+ReactDOM.render(template, appRoot);
+};
+
+render();
