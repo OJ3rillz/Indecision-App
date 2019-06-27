f0b1cc91a253d5af970ca2e3f8126352427c75b9
OJ3rillz <timitrilli@gmail.com>
2019-06-27 10:36:13 +0100

    Challenge Completed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,10 +7,12 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 var Person = function () {
    function Person() {
       var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+      var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
       _classCallCheck(this, Person);
 
       this.name = name;
+      this.age = age;
    }
 
    _createClass(Person, [{
@@ -21,13 +23,18 @@ var Person = function () {
          //instead of
          // return 'Hi.I am '+ this.name + '!' 
       }
+   }, {
+      key: 'getDescription',
+      value: function getDescription() {
+         return this.name + ' is ' + this.age + ' year(s) old.';
+      }
    }]);
 
    return Person;
 }();
 
-var me = new Person('Akinleye Oluwatimilehin');
-console.log(me.getGreating());
+var me = new Person('Akinleye Oluwatimilehin', 24);
+console.log(me.getDescription());
 
 var others = new Person();
-console.log(others.getGreating());
+console.log(others.getDescription());
--- src/playground/es6-classes1.js
+++ src/playground/es6-classes1.js
@@ -1,17 +1,22 @@
 class Person {
-   constructor(name = 'Anonymous') {
+   constructor(name = 'Anonymous', age = 0) {
        this.name = name;
+       this.age = age;
    }
+   
    getGreating() {
       //using template strings is easier i.e
       return `Hi. I am ${this.name}!`
       //instead of
       // return 'Hi.I am '+ this.name + '!' 
    }
+   getDescription() {
+         return `${this.name} is ${this.age} year(s) old.`
+   }
 }
 
-const me = new Person('Akinleye Oluwatimilehin');
-console.log(me.getGreating());
+const me = new Person('Akinleye Oluwatimilehin', 24);
+console.log(me.getDescription());
 
 const others = new Person()
-console.log(others.getGreating());
+console.log(others.getDescription());
