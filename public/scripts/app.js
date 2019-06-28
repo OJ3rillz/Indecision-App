024c63c39a760eb2b86530e7b53fa0db4734d201
OJ3rillz <timitrilli@gmail.com>
2019-06-28 15:30:42 +0100

    Progressss

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,40 +1,91 @@
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+console.log("app is running");
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var app = {
+      title: 'Help Me Decide',
+      subtitle: 'Life or Death? Put it in my hands',
+      options: []
+};
 
-var Person = function () {
-   function Person() {
-      var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
-      var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+var onFormSubmit = function onFormSubmit(e) {
+      e.preventDefault();
 
-      _classCallCheck(this, Person);
+      var option = e.target.elements.option.value;
 
-      this.name = name;
-      this.age = age;
-   }
-
-   _createClass(Person, [{
-      key: 'getGreating',
-      value: function getGreating() {
-         //using template strings is easier i.e
-         return 'Hi. I am ' + this.name + '!';
-         //instead of
-         // return 'Hi.I am '+ this.name + '!' 
+      if (option) {
+            app.options.push(option);
+            e.target.elements.option.value = ' ';
+            render();
       }
-   }, {
-      key: 'getDescription',
-      value: function getDescription() {
-         return this.name + ' is ' + this.age + ' year(s) old.';
-      }
-   }]);
+};
+var onRemoveAll = function onRemoveAll() {
+      app.options = [];
+      render();
+};
 
-   return Person;
-}();
+var onMakeDecisions = function onMakeDecisions() {
+      var randomNum = Math.floor(Math.random() * app.options.length);
+      var option = app.options[randomNum];
+      alert(option);
+};
 
-var me = new Person('Akinleye Oluwatimilehin', 24);
-console.log(me.getDescription());
+var appRoot = document.getElementById('app');
 
-var others = new Person();
-console.log(others.getDescription());
+var numbers = [55, 101, 1000];
+var render = function render() {
+      var template = React.createElement(
+            'div',
+            null,
+            React.createElement(
+                  'h1',
+                  null,
+                  app.title
+            ),
+            app.subtitle && React.createElement(
+                  'p',
+                  null,
+                  app.subtitle
+            ),
+            React.createElement(
+                  'p',
+                  null,
+                  app.options.length > 0 ? 'Here are you options' : 'No options'
+            ),
+            React.createElement(
+                  'button',
+                  { disabled: app.options.length === 0, onClick: onMakeDecisions },
+                  'What should i do '
+            ),
+            React.createElement(
+                  'button',
+                  { onClick: onRemoveAll },
+                  'Remove All'
+            ),
+            React.createElement(
+                  'ol',
+                  null,
+                  app.options.map(function (option) {
+                        return React.createElement(
+                              'li',
+                              { key: option },
+                              option
+                        );
+                  })
+            ),
+            React.createElement(
+                  'form',
+                  { onSubmit: onFormSubmit },
+                  React.createElement('input', { type: 'text', name: 'option' }),
+                  React.createElement(
+                        'button',
+                        null,
+                        'Add Option'
+                  )
+            )
+      );
+
+      ReactDOM.render(template, appRoot);
+};
+
+render();
