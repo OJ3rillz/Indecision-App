e4591b2d53697f53ea26d90554d7dbeacb96571c
OJ3rillz <timitrilli@gmail.com>
2019-06-29 13:37:24 +0100

    challenge completed

--- src/app.js
+++ src/app.js
@@ -15,16 +15,37 @@ class Action extends React.Component {
                   <div>
                   <button>What should i do?</button>
                   </div>
-            )
+            );
       }
 }
 
+class Options extends React.Component {
+      render() {
+            return (
+                  <div>
+                  <h1>Options component here</h1>
+                  </div>
+            );
+      }
+}
+
+class AddOptions extends React.Component {
+      render() {
+            return (
+                  <div>
+                  <h3>AddOption component here</h3>
+                  </div>
+            );
+      }
+}
 
 
 const jsx = (
       <div>
          <Header />
          <Action />
+         <Options />
+         <AddOptions />
       </div>
 );
 
