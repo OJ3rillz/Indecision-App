d5727c8ffab6b07d1d3d6a6d6afcbae20cea47df
OJ3rillz <timitrilli@gmail.com>
2019-08-09 20:37:50 +0100

    importing npm modules

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
-// import './utils.js'
+// // import './utils.js'
 
-// import subtract, { square, add } from './utils.js';
+// // import subtract, { square, add } from './utils.js';
 
-// console.log("app.js is running");
-// console.log(square(4));
-// console.log(add(100, 23)); 
-// console.log(subtract(100, 20));
+// // console.log("app.js is running");
+// // console.log(square(4));
+// // console.log(add(100, 23)); 
+// // console.log(subtract(100, 20));
 
-import './person.js'
-import isSenior, { isAdult, canDrink} from './person.js'
-console.log (isAdult(20));
-console.log (canDrink(30));
-console.log (isSenior(64));
+// import './person.js'
+// import isSenior, { isAdult, canDrink} from './person.js'
+// console.log (isAdult(20));
+// console.log (canDrink(30));
+// console.log (isSenior(64));
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const template = React.createElement('p', {}, 'testing 123');
+ReactDom.render(template, document.getElementById('app'));
+
+
+
+
+
+// import  validator from 'validator';
+
+// console.log(validator.isEmail('test'));
