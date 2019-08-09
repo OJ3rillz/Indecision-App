b9015fc7ea1673cb43596517f473d3543a421efb
OJ3rillz <timitrilli@gmail.com>
2019-08-09 16:23:14 +0100

    ImportExport
    Learnt about how to import and export data from  and into my js files

--- public/bundle.js
+++ public/bundle.js
@@ -67,7 +67,12 @@
 /* 0 */
 /***/ (function(module, exports) {
 
-console.log("app.js");
+// import './utils.js'
+// import { square, add } from './utils.js';
+
+// console.log("app.js is running");
+// console.log(square(4));
+// console.log(add(100, 23));
 
 /***/ })
 /******/ ]);
